9a814f1d9a6e4f07bcbf31151e9a426b3381db8b
Anoop Maurya <anoopmaurya144@gmail.com>
2023-03-03 15:16:29 +0530

    jenking_setup_2

--- jenking_setup.sh
+++ jenking_setup.sh
@@ -1,6 +1,6 @@
-docker build -t anoop4686/images:web_hosted_version1 .
+docker build -t anoop4686/images:web_host_version1 .
 docker login -u anoop4686 -p Anoop@8898
-docker push anoop4686/images:web_hosted_version1 .
+docker push anoop4686/images:web_host_version1 .
 
 kubectl apply -f pod.yaml
 kubectl apply -f deploy.yaml
