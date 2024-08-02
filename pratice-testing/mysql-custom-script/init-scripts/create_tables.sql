40a2e667f0e7f6d7c4399784a20fef84dee65b0c
Anoop Maurya <Anoopmaurya144@gmail.com>
2024-08-03 01:22:05 +0530

    new filw

---
+++ pratice-testing/mysql-custom-script/init-scripts/create_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE card (
+  id int(20) NOT NULL,
+  First-Name varchar(255) NOT NULL,
+  Last-Name varchar(255) NOT NULL,
+  location varchar(255) DEFAULT NULL,
+  );
---
+++ pratice-testing/mysql-custom-script/init-scripts/insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO card (id,  First-Name, Last-Name, location) values (1, 'Anoop', 'Maurya', 'Mumbai');
+INSERT INTO card (id,  First-Name, Last-Name, location) values (2, 'Nisha', 'Maurya', 'Mumbai');
+INSERT INTO card (id,  First-Name, Last-Name, location) values (3, 'Suraj', 'Maurya', 'Mumbai');
+INSERT INTO card (id,  First-Name, Last-Name, location) values (4, 'Khusboo', 'Maurya', 'Mumbai');
+INSERT INTO card (id,  First-Name, Last-Name, location) values (5, 'Naresh', 'Maurya', 'Mumbai');
